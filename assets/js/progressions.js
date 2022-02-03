432db1cd8caec50ee4bae14a3e5688aa81ca218a
Michael Palumbo <michaelpalumbo@users.noreply.github.com>
2022-02-03 10:41:04 -0500

    signin now works, retuning oauth token. #4

--- assets/js/progressions.js
+++ assets/js/progressions.js
@@ -1,4 +1,58 @@
+var credentials = {
+    username: null,
+    password: null
+}
+// store oauth creds here
+var sessionAuth;
+
+// clear field upon click
 $( ".username" ).click(function(event) {
-    console.log('clicked')
     $("#username").val('')
 })
+// clear field upon click
+$( ".password" ).click(function(event) {
+    $("#password").val('')
+})
+// get the username and password
+$( ".login" ).click(function(event) {
+    credentials.username = $("#username").val()
+    credentials.password = $("#password").val()
+    auth()
+    // localStorage.setItem(JSON.stringify(credentials))
+})
+
+function auth(){
+
+    fetch('https://api.hooktheory.com/v1/users/auth', {
+        method: 'POST',
+        body: JSON.stringify(credentials),
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+            
+        }
+    })
+    .then(function (resp) {
+
+        // Return the response as JSON
+        return resp.json();
+    
+    }).then(function (data) {
+    
+        // Log the API data
+        console.log('token', data);
+        if(data.name === 'Unauthorized'){
+            alert(data.message)
+        }
+        if(data.username === credentials.username){
+            // it worked
+            sessionAuth = data
+            console.log(sessionAuth)
+        }
+    }).catch(function (err) {
+    
+        // Log any errors
+        console.log('something went wrong', err);
+    
+    })
+}
